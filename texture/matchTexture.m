49980ce4c2f309d9186566f40fba4b991e658e8b
Tim Brown <tbrown126@dhcp-44-175.eecs.berkeley.edu>
2013-12-09 23:46:07 -0800

    matchTexture now uses a steerable pyramid.

--- texture/matchTexture.m
+++ texture/matchTexture.m
@@ -1,18 +1,45 @@
 function [ im ] = matchTexture( noise, texture, numLevelsPyr, iterations )
 im = matchHistogram(noise, texture);
-analysis_pyr = genPyr(texture, 'lap', numLevelsPyr);
+[analysis_pyr, analysis_pind] = build(texture, numLevelsPyr);
+[num_pyr , ~] = size(analysis_pind);
 
 for i = 1:iterations
     i
-    synthesis_pyr = genPyr(im, 'lap', numLevelsPyr);
-    for j = 1:numLevelsPyr
+    [synthesis_pyr, synthesis_pind] = build(im, numLevelsPyr);
+    
+    for j = 1:num_pyr
         synthesis_pyr{j} = matchHistogram(synthesis_pyr{j}, analysis_pyr{j});
+        
     end
     
-    im = collapsePyramid(synthesis_pyr);
+    im = collapse(synthesis_pyr, synthesis_pind);
     im = matchHistogram(im, texture);
 end
 
 
 end
 
+
+function [myCell, pind] = build(im, numLevelsPyr)
+[pyr,pind,~,~] = buildSpyr(im, numLevelsPyr, 'sp1Filters', 'reflect1');
+[m, b] = size(pind);
+myCell = cell(m,1);
+startOfpyr = 0;
+for x =1:m
+    product = prod(pind(x,:));
+    myCell{x} = pyr(startOfpyr+1:(product+startOfpyr));
+    startOfpyr = startOfpyr + product;
+end
+
+end
+
+
+function im = collapse(myCell, pind)
+[m, b] = size(pind);
+pyr = myCell{1}(:);
+for x =2:m
+    temp = myCell{x};
+    pyr = [pyr; temp(:)];
+end
+im = reconSpyr(pyr, pind, 'sp1Filters');
+end
